bc5ee8e9a9cc48a238877fd27198568cfde50c3a
Shahid3378 <shahidraja3378@gamil.com>
2024-12-15 12:47:54 +0530

    Issue book

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from crud import add_book, get_book
+from crud import add_book, get_book, add_member, get_member, issue_book
 
 def printBooks():
     books = get_book()
@@ -11,18 +11,39 @@ def printBooks():
         available = "Available" if book.count > 0 else "Not Available"
         print(f"{book.id}: '{book.title}' by {book.author} (ISBN: {book.isbn}) - {available} ({book.count} copies)")
 
-def addNewBook()
+def addNewBook():
     title = input("Enter book title: ")
     author = input("Enter book author: ")
     isbn = input("Enter ISBN: ")
     count = int(input("Enter number of copies: "))
     add_book(title, author, isbn, count)
+    print("=> New book added")
 
+def addNewMember():
+    name = input("Enter member name: ")
+    email = input("Enter member emil: ")
+    add_member(name, email)
+    print("=> New member added")
+
+def printMember():
+    members = get_member()
+    for member in members:
+        print(f"{member.id}: {member.name} (Email: {member.email})")
+
+def issueABook():
+    book_id = int(input("Enter book ID: "))
+    member_id = int(input("Enter member ID: "))
+    issue_book(book_id, member_id)
 
 def main():
     print("*******************************")
     print("1. Add Book")
     print("2. View Book")
+    print("3. Add Member")
+    print("4. View Member")
+    print("5. Issue Book")
+    print("6. Return Book")
+    print("7. View Transcations by Member")
     print("*******************************")
     choice = input("Enter your choice: ")
 
@@ -30,6 +51,17 @@ def main():
         addNewBook()
     elif choice == "2":
         printBooks()
+    elif choice == "3":
+        addNewMember()
+    elif choice == "4":
+        printMember()
+    elif choice == "5":
+        issueABook()
+    elif choice == "6":
+        pass
+    elif choice == "7":
+        pass
+    
 
 if __name__ == "__main__":
     main()
